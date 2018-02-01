4d0b4a11d516e3dc6d7b218a57a6fc28d02068ca
Zachary Coursey <zcoursey22@gmail.com>
2018-02-01 23:41:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ var recipes = {};
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, {[key]: value});
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+}
